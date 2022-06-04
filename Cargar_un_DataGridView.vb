cc86bdb63b446e16ee4106a626b9cc39ae1d0194
Samuel Gomez <106413475+Dev-SamGomez@users.noreply.github.com>
2022-06-03 19:14:18 -0600

    Add files via upload

--- Cargar_un_DataGridView.vb
+++ Cargar_un_DataGridView.vb
@@ -2,39 +2,39 @@
 Imports System.Data.SqlClient 'Libreria para uso de clases de SQL
 
 Private Sub LoadGrid()
-        Try
-            Dim query As String = $"select * from Vendors where IDVendor = '{VendorID}'" 'Iniciamos con el query
-            Dim Table As Datatable
-            Dim cmd As SqlCommand
-            Dim dr As SqlDataReader
-            cmd = New SqlCommand(query, cnn) 'En la clase de SqlCommand en su constructor, enviamos el query y la conexion a nuestra DB
-            cmd.CommandType = CommandType.Text 'Seleccionamos el tipo de comando, en este caso el comando sera tipo texto
-            cnn.Open()
-            dr = cmd.ExecuteReader 'Al abrir la conexion, el datareader lo inicializamos con la ejecucion del comando de sql, con la funcion ExecuteReader
-            Table.Load(dr) 'A la funcion Load de la clase Datatable, enviamos el datareader como parametro, para que con este se cargue el Datatable
-            cnn.Close()
-            If Table.Rows.Count > 0 Then 'Validamos si hay registros en el Datatable que acabamos de cargar, si hay datos, cargamos la Grid
+    Try
+        Dim query As String = $"SELECT * FROM Vendors WHERE IDVendor = '{VendorID}'" 'Iniciamos con el query
+        Dim Table As Datatable
+        Dim cmd As SqlCommand
+        Dim dr As SqlDataReader
+        cmd = New SqlCommand(query, cnn) 'En la clase de SqlCommand en su constructor, enviamos el query y la conexion a nuestra DB
+        cmd.CommandType = CommandType.Text 'Seleccionamos el tipo de comando, en este caso el comando sera tipo texto
+        cnn.Open()
+        dr = cmd.ExecuteReader 'Al abrir la conexion, el datareader lo inicializamos con la ejecucion del comando de sql, con la funcion ExecuteReader
+        Table.Load(dr) 'A la funcion Load de la clase Datatable, enviamos el datareader como parametro, para que con este se cargue el Datatable
+        cnn.Close()
+        If Table.Rows.Count > 0 Then 'Validamos si hay registros en el Datatable que acabamos de cargar, si hay datos, cargamos la Grid
             YouDataGridView.DataSource = Table 'Con la propiedad DataSource de la Grid, seleccionamos la Datatable que cargamos
         Else
             YouDataGridView.DataSource = Nothing
         End If
-        Catch ex As Exception
-            cnn.Close()
-            MessageBox.Show(ex.Message + vbNewLine + ex.ToString)
-        End Try
+    Catch ex As Exception
+        cnn.Close()
+        MessageBox.Show(ex.Message + vbNewLine + ex.ToString)
+    End Try
 End Sub
 
 'Cargar un DataGridView con una Lista
 Private Sub LoadGridWithList()
-        Try
-            If ListItems.Count > 0 Or ListItems IsNot Nothing Then 'Verificamos mediante una validacion que la lista con la que llenaremos la Grid no este vacia.
+    Try
+        If ListItems.Count > 0 Or ListItems IsNot Nothing Then 'Verificamos mediante una validacion que la lista con la que llenaremos la Grid no este vacia.
             YouDataGridView.DataSource = ListItems.ToList() 'Si la lista contiene mas de 0 elementos entonces usamos la propiedad de la Grid DataSource y la cargamos con la lista.
         Else
             YouDataGridView.DataSource = Nothing
-        End IF
-        Catch ex As Exception
-            MessageBox.Show(ex.Message + vbNewLine + ex.ToString)
-        End Try
+        End If
+    Catch ex As Exception
+        MessageBox.Show(ex.Message + vbNewLine + ex.ToString)
+    End Try
 End Sub
 
 'Cargar un DatagridView desde un archivo de Excel
@@ -45,7 +45,7 @@ Private Sub LoadGridByExcel()
         ' de Excel y, pasemos la informacion a la DataGridView
         Dim Table As Datatable = New Datatable()
         Dim OpenFileDlg As New Windows.Forms.OpenFileDialog ' Generamos la Clase OpenFileDialog
-        Dim result As DialogResult = OpenFileDlg.ShowDialog() ' El archivo seleccionado, lo grabaremos en una variable de la Clase Dialog Result
+        Dim result As DialogResult = OpenFileDlg.ShowDialog() ' El aarchivo seleccionado, lo grabaremos en una variable de la Clase Dialog Result
         Dim path As String = OpenFileDlg.FileName 'Este guardamos el Path
         Dim archivo As String = path.ToString
         OpenFileDlg = Nothing 'Matamos el Open Dialog
@@ -67,6 +67,40 @@ Private Sub LoadGridByExcel()
             YouDataGridView.DataSource = Nothing
         End If
     Catch ex As Exception
+        MessageBox.Show(ex.Message)
+    End Try
+End Sub
 
+'Cargar una DataGridView con un filtro 
+' En este ejemplo, la problematica a resolver es, cada vez que el usuario escriba en un TextBox, este vaya filtrando en una Query directo a la 
+' DB y a su vez, los resultados se vayan reflejando en la DataGridView
+
+Private Sub txtBuscador_TextChanged_1(sender As Object, e As EventArgs) Handles txtBuscador.TextChanged
+    ' Cada vez que ingrese un dato el usuario, esta al ser el event Changed del TextBox, enviara al Metodo filterData
+    Cursor.Current = System.Windows.Forms.Cursors.WaitCursor
+    Dim filter As String = CType(sender, TextBox).Text
+    If filter.Trim() <> String.Empty Then
+        filterData(filter)
+    End If
+    Cursor.Current = System.Windows.Forms.Cursors.Default
+End Sub
+Public Sub filtrarDatos(ByVal buscar As String)
+    Try
+        Dim da1 As New SqlDataAdapter ' Declaramos la clase SqlDataAdapter
+        Dim query As String = "SELECT * FROM Costumers WHERE Costumer like @filter" ' Generamos la query
+        da1 = New SqlDataAdapter(query, cnn)
+        da1.SelectCommand.Parameters.AddWithValue("@filter", String.Format("%{0}%", buscar))
+        ' Enviamos los parametros, el primero siendo el que se encuentra en la query
+        ' El segundo el formato que se le dara, usando REGEX, el Tercero el parametro que enviamos desde que el evento TextChanged le envio a este metodo
+
+        Dim table As New DataTable
+        da1.Fill(table) 'Llenamos el DataTable
+
+        With YouDataGridView
+            .DataSource = table
+            .ClearSelection()
+        End With
+    Catch ex As Exception
+        MessageBox.Show(ex.Message)
     End Try
 End Sub
